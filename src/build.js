4ad68ba2fba78a2d3f89ea7b2d8a62e2bd753a34
Charlie Jackson <charlie@charliejackson.com>
2017-11-05 15:21:52 +0000

    If a video does not have an audio track then add one

--- src/build.js
+++ src/build.js
@@ -1,27 +1,83 @@
 import ffmpeg from 'fluent-ffmpeg';
 import { join } from 'path';
+import { emptydir, copy } from 'fs-extra';
+import { exec } from 'child_process';
 import items from 'tmp/combine/items.json';
 
-function mediaPath(name) {
-  return join(__dirname, `../tmp/combine/${name}`);
+function mediaPath(pre, name) {
+  return join(__dirname, `../tmp/${pre ? 'combine' : 'process'}/${name}`);
 }
 
-const command = ffmpeg();
+function prepareFile({ name }) {
+  return new Promise((resolve, reject) => {
+    const itemPath = mediaPath(true, name);
 
-items.forEach(({ name }) => {
-  if (name.includes('.mp4')) {
-    command.mergeAdd(mediaPath(name));
-  }
-});
+    ffmpeg.ffprobe(itemPath, (err, metadata) => {
+      if (err) return reject(err);
 
-command
-  .on('progress', (progress) => {
-    console.log(`Processing: ${Math.floor(progress.percent)}% done`);
-  })
-  .on('end', () => {
-    console.log('Finished processing');
-  })
-  .mergeToFile(
-    join(__dirname, '../tmp/output.mp4'),
-    join(__dirname, '../tmp/processing')
-  );
+      let hasAudio = false;
+
+      metadata.streams.forEach(({ codec_type }) => {
+        if (codec_type === 'audio') {
+          hasAudio = true;
+        }
+      });
+
+      const outputPath = mediaPath(false, name);
+
+      if (hasAudio) {
+        return copy(itemPath, outputPath).then(resolve);
+      }
+
+      exec(
+        `ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i ${itemPath} -shortest -c:v copy -c:a aac ${outputPath}`,
+        {},
+        (error) => {
+          if (error) return reject(error);
+
+          return resolve();
+        }
+      );
+    });
+  });
+}
+
+function init() {
+  const itemsToProcess = [];
+
+  items.forEach((item) => {
+    if (item.name.includes('.mp4')) {
+      itemsToProcess.push(item);
+    }
+  });
+
+  const promises = [];
+
+  itemsToProcess.forEach((item) => {
+    promises.push(prepareFile(item));
+  });
+
+  Promise.all(promises).then(() => {
+    const command = ffmpeg();
+
+    itemsToProcess.forEach(({ name }) => {
+      if (name.includes('.mp4')) {
+        command.input(mediaPath(false, name));
+      }
+    });
+
+    command
+      .on('progress', (progress) => {
+        console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+      })
+      .on('end', () => {
+        console.log('Finished processing');
+      })
+      .mergeToFile(
+        join(__dirname, '../tmp/output.mp4'),
+        join(__dirname, '../tmp/processing')
+      );
+  });
+}
+
+emptydir(join(__dirname, '../tmp/process')).then(init);
